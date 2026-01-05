b0ccab71ea7e4c4bb7ebe8d48d272d92f8c31571
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-05 20:53:38 +0600

    installed and impleted bettter auth

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import expres from "express";
 import { postRouter } from "./modules/posts/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = expres();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(expres.json());
 
 app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello world!!");
+  res.send("Prisma Blog Application Server!!");
 });
 
 export default app;
