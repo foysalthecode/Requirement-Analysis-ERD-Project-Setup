8529ea75018a217a05c705a4b88a89cd2ab08f19
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-08 04:16:44 +0600

    implemented seed admin function

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middleware/auth";
+
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: "Admin saheb",
+      email: "admin@admin.com",
+      role: UserRole.ADMIN,
+      password: "admin1234",
+    };
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("User already exists!!!");
+    }
+
+    const signUpAdmin = await fetch(
+      "http://localhost:5000/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(adminData),
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
